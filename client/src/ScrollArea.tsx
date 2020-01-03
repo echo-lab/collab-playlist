d1ef68058e2982ec3d16e4b73352714f9ff4a5fc
Noam Bendelac <bendelac.noam@gmail.com>
2020-01-02 23:21:13 -0500

    Refactor and document useDimensions

--- client/src/ScrollArea.tsx
+++ client/src/ScrollArea.tsx
@@ -7,15 +7,36 @@ import './scrollbar.css'
 
 type Rect = ReturnType<Element['getBoundingClientRect']>
 
-const useDimensions = ():[MutableRefObject<HTMLDivElement>, null | Rect, boolean] => {
+/**
+ * Gives dimensions of an HTML element
+ * 
+ * Before initial render, the HTML element doesn't exist yet so ref.current is
+ * undefined and there is no meaningful "size"; thus a default rect is used
+ * 
+ * To the best of my knowledge, does not update when the HTML element's size
+ * updates; for this you would need some kind of useExternalState on the
+ * element's size (for now we warn the user not to resize window)
+ * 
+ * Creates its own ref; to use ref with other hooks, either use the returned ref
+ * or rewrite hook to use ref given in argument instead of useRef
+ */
+const useDimensions = (): [MutableRefObject<HTMLDivElement>, Rect] => {
+  const defaultRect = {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: 0,
+    height: 0,
+  } as Rect // missing x, y, and toJSON from DOMRect, but that's because those
+  // don't exist in ClientRect
+  
   const ref = useRef<HTMLDivElement>(null)
-  const [dimensions, setDimensions] = useState<null | Rect>(null)
-  const [rendered, setRendered] = useState(false)
+  const [dimensions, setDimensions] = useState<Rect>(defaultRect)
   
   useLayoutEffect(() => {
-    // console.log('resize')
+    console.log('for dimensions: layout effect and update dimensions')
     setDimensions(ref.current.getBoundingClientRect())
-    setRendered(true)
   }, [])
 
   // useEffect(() => {
@@ -24,16 +45,15 @@ const useDimensions = ():[MutableRefObject<HTMLDivElement>, null | Rect, boolean
   //   }, 0)
   // }, [])
 
-  return [ref, dimensions, rendered]
+  return [ref, dimensions]
 }
 
 export const ScrollArea = ({ style, children }) => {
   
-  const [ref, dimensions, rendered] = useDimensions()
-  
-  const { height } = rendered ? (dimensions as Rect) : { height: 0 }
+  const [ref, { height }] = useDimensions()
   
   // console.log({ height })
+  console.log(`for dimensions: render component with height: ${height}`)
 
   return <div style={style} ref={ref}>
     <SimpleBar style={{ height: height }}>
