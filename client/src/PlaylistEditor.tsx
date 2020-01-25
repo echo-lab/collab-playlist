e838c9dc8216c0b1d95d51ca43b09b2d92aaa8ad
Noam Bendelac <bendelac.noam@gmail.com>
2020-01-25 13:07:00 -0500

    Refactor usePlaylistData

--- client/src/PlaylistEditor.tsx
+++ client/src/PlaylistEditor.tsx
@@ -1,20 +1,28 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 // import { ScrollArea } from './ScrollArea'
 import { useParams } from 'react-router-dom'
-import { useApi } from './api-hooks'
+import { useResource, apiWrapper } from './api-hooks'
 import { classes } from './styles'
 
 
-const usePlaylistData = (id) => {
-  return useApi(`/api/playlists/${id}/`) as SpotifyApi.PlaylistObjectFull
+const usePlaylistData = (id: string) => {
+  // return useApi(`/api/playlists/${id}/`) as SpotifyApi.PlaylistObjectFull
+  const [resource, setters] = useResource<SpotifyApi.PlaylistObjectFull>(null)
+  
+  useEffect(() => {
+    apiWrapper(`/api/playlists/${id}/`, setters)
+    console.log('TEST', id, setters)
+  }, [id, setters])
+  
+  return resource
 }
 
 
 export const PlaylistEditor = () => {
   const { id } = useParams()
   
-  const data = usePlaylistData(id)
+  const [data, , ] = usePlaylistData(id)
   
   console.log({data})
   
