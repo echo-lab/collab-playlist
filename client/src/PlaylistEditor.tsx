63aac13af9aac319bc0f9f9f6110c23e6e46532e
Noam Bendelac <bendelac.noam@gmail.com>
2020-01-26 00:49:09 -0500

    Add to song render and layout

--- client/src/PlaylistEditor.tsx
+++ client/src/PlaylistEditor.tsx
@@ -1,9 +1,10 @@
 
-import React, { useEffect } from 'react'
+import React, { useEffect, CSSProperties } from 'react'
 // import { ScrollArea } from './ScrollArea'
 import { useParams } from 'react-router-dom'
 import { useResource, apiWrapper } from './apiWrapper'
-import { classes } from './styles'
+import { classes, colors } from './styles'
+import { useHover } from './useHover'
 
 
 const usePlaylistData = (id: string) => {
@@ -24,7 +25,12 @@ export const PlaylistEditor = () => {
   
   console.log({data, loading})
   
-  return <div>
+  const playlistEditorStyle: CSSProperties = {
+    ...classes.column,
+    padding: '2.0rem',
+  }
+  
+  return <div style={playlistEditorStyle}>
     { loading
     ? null
     : data.tracks.items.map((item, index) => 
@@ -36,8 +42,49 @@ export const PlaylistEditor = () => {
 
 
 const SongRow = ({ item }: { item: SpotifyApi.PlaylistTrackObject }) => {
-  return <div>
-    {item.added_by.id}
+  const { track } = item
+  // track.name
+  const artistNames = track.artists.map(artist => artist.name).join(', ')
+  
+  const [isHovered, hoverContainerProps] = useHover()
+  
+  const fontSize = '1.8rem'
+  
+  const rowStyle: CSSProperties = {
+    // display: 'contents',
+    ...classes.row,
+    // justifyContent: 'spaceEvenly',
+    padding: '0.4rem 1.4rem',
+    ...(isHovered && { background: colors.grayscale.darkGray}),
+  }
+  const childStyle: CSSProperties = {
+    ...classes.text,
+    ...classes.textOverflow(),
+    fontSize,
+    margin: '0 1.4rem',
+  }
+  const titleStyle: CSSProperties = {
+    ...childStyle,
+    flex: 2,
+  }
+  const artistStyle: CSSProperties = {
+    ...childStyle,
+    flex: 1,
+  }
+  const albumStyle: CSSProperties = {
+    ...childStyle,
+    flex: 1,
+  }
+  const addedByStyle: CSSProperties = {
+    ...childStyle,
+    flex: 1,
+  }
+  
+  return <div style={rowStyle} {...hoverContainerProps}>
+    <div style={titleStyle}>{track.name}</div>
+    <div style={artistStyle}>{artistNames}</div>
+    <div style={albumStyle}>{track.album.name}</div>
+    <div style={addedByStyle}>{item.added_by.id}</div>
   </div>
 }
 
