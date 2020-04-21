90b0134e8eb0de1c300a1bbe9c04dae275778113
Noam Bendelac <bendelac.noam@gmail.com>
2020-04-21 18:04:23 -0400

    Add SeparateChat component

---
+++ client/src/Playlist-Editor/SeparateChat.tsx
@@ -0,0 +1,40 @@
+
+import React, { CSSProperties } from 'react'
+import { classes } from '../styles'
+// import * as styles from './playlistTableRowStyles'
+// import { State } from './modificationReducer'
+import { SeparateMessageEditor } from './MessageEditor'
+
+
+
+// const chatStyle = {
+//   ...classes.column,
+//   // margin: '0 1.0rem 1.0rem',
+// }
+
+
+export const SeparateChat = ({
+  chat,
+  reloadPlaylist,
+  style,
+}: {
+  chat: any[],
+  reloadPlaylist: () => void,
+  style: CSSProperties,
+}) => {
+  
+  const chatStyle = {
+    ...classes.column,
+    ...style,
+  }
+  
+  return <div style={chatStyle}>
+    <div>
+      {/* TODO chat history */}
+    </div>
+    <div>
+      <SeparateMessageEditor reloadPlaylist={reloadPlaylist} />
+    </div>
+  </div>
+}
+
